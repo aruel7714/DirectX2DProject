9e8e607d64fe1e83dc610131860b42d5f1fe4c69
aruel7714 <harunia7714@daum.net>
2023-09-05 17:06:15 +0900

    디바이스 초기화 및 스왑체인 초기화

--- DirectX2D/GameEngineCore/GameEngineDevice.h
+++ DirectX2D/GameEngineCore/GameEngineDevice.h
@@ -24,24 +24,32 @@ public:
 	GameEngineDevice& operator=(GameEngineDevice && _Other) noexcept = delete;
 
 	// 윈도우에서 만들었기 때문에
-	// 근간인 HWND를 많이 요구합ㄴ디ㅏ.
+	// 근간인 HWND를 많이 요구합니다.
 	// 그런데 선생님 구조는 그 window를 또 감싼다.
 	void Initiallize(const class GameEngineWindow& _Window);
 
 protected:
 
 private:
+	const GameEngineWindow* Window;
+
 	IDXGIAdapter* GetHighPerformanceAdapter();
 
 	// 그래픽카드의 메모리관련 접근권한 인터페이스
 	// Ex) 이미지 정점을 만들고 지울수 있는 권한이다.
-
+	// 뭔가를 만들려면 디바이스
 	ID3D11Device* Device = nullptr;
 
 	// 그래픽카드의 연산(그린다)관련 접근권한 인터페이스
 	// 로드된 이미지나 만들어진 정점을 화면에 그려라
-
+	// 뭔가를 그릴려면 컨텍스트
 	ID3D11DeviceContext* Context = nullptr;
 
+	// 화면 출력 처리용 인터페이스
+	IDXGISwapChain* SwapChain = nullptr;
+
+	std::shared_ptr<class GameEngineTexture> BackBufferTexture;
+
+	void CreateSwapChain();
 };
 
