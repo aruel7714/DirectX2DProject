b6e392df1dabbcea978911505c39c10095fc4aa3
aruel7714 <harunia7714@daum.net>
2023-09-11 19:21:07 +0900

    타이틀 구름 이동 구현중, 타일맵 제작중, 프로젝트 설정 변경

--- DirectX2D/GameEngineContents/BackCloud.cpp
+++ DirectX2D/GameEngineContents/BackCloud.cpp
@@ -15,18 +15,43 @@ void BackCloud::Start()
 		Renderer = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
 		Renderer->SetSprite("BackCloud2.png");
 
+		Renderer2 = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
+		Renderer2->SetSprite("BackCloud2.png");
+
 		std::shared_ptr<GameEngineTexture> Texture = GameEngineTexture::Find("BackCloud2.png");
 
-		
-		
 		float4 HScale = Texture->GetScale().Half();
 		HScale.X -= 640.0f;
 		HScale.Y *= 0.0f;
 
+		float4 HScale2 = Texture->GetScale();
+		HScale2.X += HScale.X;
+		HScale2.Y *= 0.0f;
+
+		/*float4 HScale2 = HScale;
+		HScale2.X = 640.0f + 1280.0f;*/
+
 		Renderer->Transform.SetLocalPosition(HScale);
+		Renderer2->Transform.SetLocalPosition(HScale2);
 	}
 }
 void BackCloud::Update(float _Delta)
 {
+	float4 Pos = Renderer->Transform.GetWorldPosition();
+	int a = 0;
 
+	float Speed = 50.0f;
+
+	Renderer->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+	Renderer2->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+
+	//if (-640.0f <= Renderer->Transform.GetWorldPosition().X)
+	//{
+	//	Renderer->Transform.SetLocalPosition({ 640.0f + 1280.0f, 0.0f });
+	//}
+
+	//if (-640.0f <= Renderer2->Transform.GetWorldPosition().X)
+	//{
+	//	Renderer2->Transform.SetLocalPosition({ 640.0f + 1280.0f, 0.0f });
+	//}
 }
--- DirectX2D/GameEngineContents/BackCloud.h
+++ DirectX2D/GameEngineContents/BackCloud.h
@@ -20,6 +20,6 @@ protected:
 	void Update(float _Delta) override;
 private:
 	std::shared_ptr<class GameEngineSpriteRenderer> Renderer;
+	std::shared_ptr<class GameEngineSpriteRenderer> Renderer2;
 
 };
-
