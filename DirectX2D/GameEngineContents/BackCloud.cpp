377c56236bd617e87cf37e55be40f06237244255
aruel7714 <harunia7714@daum.net>
2023-09-18 20:34:42 +0900

    TitleLevel BackCloud 움직임 구현

--- DirectX2D/GameEngineContents/BackCloud.cpp
+++ DirectX2D/GameEngineContents/BackCloud.cpp
@@ -14,41 +14,51 @@ void BackCloud::Start()
 	{
 		Renderer = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
 		Renderer->SetSprite("BackCloud2.png");
-		//Renderer->SetImageScale(float4{ 500 , 500 });
-		/*Renderer2 = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
-		Renderer2->SetSprite("BackCloud2.png");*/
+		Renderer2 = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
+		Renderer2->SetSprite("BackCloud2.png");
 
 		std::shared_ptr<GameEngineTexture> Texture = GameEngineTexture::Find("BackCloud2.png");
 
-		float4 HScale = Texture->GetScale().Half();
+		float4 Scale = Texture->GetScale();
+
+		float4 HScale = Scale.Half();
 		HScale.Y *= -1.0f;
 
-		Transform.SetLocalPosition(HScale);
+		//Transform.SetLocalPosition(HScale);
 
 		//float4 HScale = Texture->GetScale().Half();
 		//HScale.X -= 0.0f;
 		//HScale.Y *= 0.0f;
 
-		/*float4 HScale2 = Texture->GetScale();
-		HScale2.X += HScale.X;
-		HScale2.Y *= 0.0f;*/
+		float4 HScale2 = HScale;
+		HScale2.X += Scale.X;
+		HScale2.X -= 1.0f;
 
 		/*float4 HScale2 = HScale;
 		HScale2.X = 640.0f + 1280.0f;*/
 
-		//Renderer->Transform.SetLocalPosition(HScale);
-		//Renderer2->Transform.SetLocalPosition(HScale2);
+		Renderer->Transform.SetLocalPosition(HScale);
+		Renderer2->Transform.SetLocalPosition(HScale2);
 	}
 }
 void BackCloud::Update(float _Delta)
 {
-	float4 Pos = Renderer->Transform.GetWorldPosition();
-	int a = 0;
-
 	float Speed = 50.0f;
 
-	//Renderer->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
-	//Renderer2->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+	Renderer->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+	Renderer2->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+
+	float4 Scale = Renderer->GetCurSprite().Texture->GetScale();
+
+	if (-Scale.Half().X >= Renderer->Transform.GetWorldPosition().X)
+	{
+		Renderer->Transform.SetLocalPosition({ Scale.Half().X + Scale.X - 2.0f, -Scale.Half().Y });
+	}
+
+	if (-Scale.Half().X >= Renderer2->Transform.GetWorldPosition().X)
+	{
+		Renderer2->Transform.SetLocalPosition({ Scale.Half().X + Scale.X - 2.0f, -Scale.Half().Y });
+	}
 
 	//if (-640.0f <= Renderer->Transform.GetWorldPosition().X)
 	//{
