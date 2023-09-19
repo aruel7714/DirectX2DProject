5a9af6e2670616b979824984f9f1398a25594c51
aruel7714 <harunia7714@daum.net>
2023-09-19 15:08:38 +0900

    TitleLevel FrontCloud 이동 구현

--- DirectX2D/GameEngineContents/FrontCloud.cpp
+++ DirectX2D/GameEngineContents/FrontCloud.cpp
@@ -14,20 +14,40 @@ void FrontCloud::Start()
 	{
 		Renderer = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
 		Renderer->SetSprite("FrontCloud2.png");
+		Renderer2 = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::BackGround);
+		Renderer2->SetSprite("FrontCloud2.png");
 
 		std::shared_ptr<GameEngineTexture> Texture = GameEngineTexture::Find("FrontCloud2.png");
 
-
+		float4 Scale = Texture->GetScale();
 
 		float4 HScale = Texture->GetScale().Half();
-		//HScale.X -= 640.0f;
-		//HScale.Y *= 0.0f;
 		HScale.Y *= -1.0f;
 
-		Transform.SetLocalPosition(HScale);
+		float4 HScale2 = HScale;
+		HScale2.X += Scale.X;
+		HScale2.X -= 1.0f;
+
+		Renderer->Transform.SetLocalPosition(HScale);
+		Renderer2->Transform.SetLocalPosition(HScale2);
 	}
 }
 void FrontCloud::Update(float _Delta)
 {
+	float Speed = 100.0f;
 
+	Renderer->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+	Renderer2->Transform.AddLocalPosition(float4::LEFT * _Delta * Speed);
+
+	float4 Scale = Renderer->GetCurSprite().Texture->GetScale();
+
+	if (-Scale.Half().X >= Renderer->Transform.GetWorldPosition().X)
+	{
+		Renderer->Transform.SetLocalPosition({ Scale.Half().X + Scale.X - 2.0f, -Scale.Half().Y });
+	}
+
+	if (-Scale.Half().X >= Renderer2->Transform.GetWorldPosition().X)
+	{
+		Renderer2->Transform.SetLocalPosition({ Scale.Half().X + Scale.X - 2.0f, -Scale.Half().Y });
+	}
 }
