736c8a4ebeda61b020cd0b72c89d024f7bee47a3
aruel7714 <harunia7714@daum.net>
2023-09-25 17:04:23 +0900

    엔진 업데이트 - Debug

--- DirectX2D/GameEngineBase/GameEngineDebug.h
+++ DirectX2D/GameEngineBase/GameEngineDebug.h
@@ -3,26 +3,31 @@
 #include <assert.h>
 
 //// Ό³Έν :
-class GameEngineDebug
+//class GameEngineDebug
+//{
+//public:
+//	// constrcuter destructer
+//	GameEngineDebug();
+//	~GameEngineDebug();
+//
+//	// delete Function
+//	GameEngineDebug(const GameEngineDebug& _Other) = delete;
+//	GameEngineDebug(GameEngineDebug&& _Other) noexcept = delete;
+//	GameEngineDebug& operator=(const GameEngineDebug& _Other) = delete;
+//	GameEngineDebug& operator=(GameEngineDebug&& _Other) noexcept = delete;
+//
+//	static void LeakCheck();
+//
+//protected:
+//
+//private:
+//
+//};
+
+namespace GameEngineDebug
 {
-public:
-	// constrcuter destructer
-	GameEngineDebug();
-	~GameEngineDebug();
-
-	// delete Function
-	GameEngineDebug(const GameEngineDebug& _Other) = delete;
-	GameEngineDebug(GameEngineDebug&& _Other) noexcept = delete;
-	GameEngineDebug& operator=(const GameEngineDebug& _Other) = delete;
-	GameEngineDebug& operator=(GameEngineDebug&& _Other) noexcept = delete;
-
-	static void LeakCheck();
-
-protected:
-
-private:
-
-};
+	void LeakCheck();
+}
 
 #define MsgBoxAssert(Text) \
 std::string Value = Text; \
