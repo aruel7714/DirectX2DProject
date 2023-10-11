7bd85cfaa9eb2ff8bda6682ef75a4bfce9d40a12
aruel7714 <harunia7714@daum.net>
2023-10-11 17:06:46 +0900

    엔진 업데이트

--- DirectX2D/GameEngineCore/GameEngineDebugCore.cpp
+++ DirectX2D/GameEngineCore/GameEngineDebugCore.cpp
@@ -50,6 +50,7 @@ void GameEngineDebug::DrawSphere2D(const GameEngineTransform& _Trans, float4 _Co
 }
 void GameEngineDebug::DrawSphere2D(float4 _Scale, float4 _Rot, float4 _Pos, float4 _Color /*= float4::RED*/, class GameEngineCamera* _Camera /*= nullptr*/)
 {
+	_Scale.Y = _Scale.Z = _Scale.X;
 	GameEngineDebug::DrawMesh("Sphere", _Scale, _Rot, _Pos, _Color, _Camera);
 }
 
