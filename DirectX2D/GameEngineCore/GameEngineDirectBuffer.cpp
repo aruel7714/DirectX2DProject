191b509e46e477081005e0264808d8310b61e4e6
aruel7714 <harunia7714@daum.net>
2023-11-21 14:26:10 +0900

    엔진 스트럭처드 버퍼 시작

--- DirectX2D/GameEngineCore/GameEngineDirectBuffer.cpp
+++ DirectX2D/GameEngineCore/GameEngineDirectBuffer.cpp
@@ -8,10 +8,14 @@ GameEngineDirectBuffer::GameEngineDirectBuffer()
 
 GameEngineDirectBuffer::~GameEngineDirectBuffer()
 {
+	BufferRelease();
+}
+
+void GameEngineDirectBuffer::BufferRelease()
+{
 	if (nullptr != Buffer)
 	{
 		Buffer->Release();
 		Buffer = nullptr;
 	}
 }
-
