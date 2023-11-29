5639d7a53e7b137753a22cd4c8ccdf3c47166e95
aruel7714 <harunia7714@daum.net>
2023-11-29 22:03:02 +0900

    EndingFloor 초안

---
+++ DirectX2D/GameEngineContents/EndingFloor.cpp
@@ -0,0 +1,19 @@
+#include "PreCompile.h"
+#include "EndingFloor.h"
+
+EndingFloor::EndingFloor()
+{
+}
+
+EndingFloor::~EndingFloor()
+{
+}
+
+void EndingFloor::Start()
+{
+
+}
+void EndingFloor::Update(float _Delta)
+{
+
+}
---
+++ DirectX2D/GameEngineContents/EndingFloor.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "BackGround.h"
+
+// Ό³Έν : 
+class EndingFloor : public BackGround
+{
+public:
+	// constrcuter destructer
+	EndingFloor();
+	~EndingFloor();
+
+	// delete Function
+	EndingFloor(const EndingFloor & _Other) = delete;
+	EndingFloor(EndingFloor && _Other) noexcept = delete;
+	EndingFloor& operator=(const EndingFloor & _Other) = delete;
+	EndingFloor& operator=(EndingFloor && _Other) noexcept = delete;
+
+protected:
+	void Start() override;
+	void Update(float _Delta) override;
+private:
+
+};
+
